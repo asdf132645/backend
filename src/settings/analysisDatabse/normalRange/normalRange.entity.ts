6b9647f32192854fe462af6f3ff078f2f3ea761a
qooktree <qooktree@gmail.com>
2024-06-18 11:11:09 +0900

    0.0.89v_(normalRange 테이블명 오류 수정)

--- src/settings/analysisDatabse/normalRange/normalRange.entity.ts
+++ src/settings/analysisDatabse/normalRange/normalRange.entity.ts
@@ -1,7 +1,7 @@
 // src/wbc-hot-keys/wbc-hot-keys.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity('wbc_normal_range_setting')
+@Entity('normal_range_setting')
 export class NormalRange {
   @PrimaryGeneratedColumn()
   id: number;
