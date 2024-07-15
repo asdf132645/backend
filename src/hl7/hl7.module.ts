bb9cc7ba587898d516d1c9b6b8e3c4ccd4cbe1ff
henry <kaynekaynekayne@github.com>
2024-07-15 13:15:40 +0900

    0.0.46v_(hl7 원상복구)

--- src/hl7/hl7.module.ts
+++ src/hl7/hl7.module.ts
@@ -1,14 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HL7Controller } from './hl7.controller';
 import { HL7Service } from './hl7.service';
-import { PlainTextMiddleware } from './plainTextMiddleware';
 
 @Module({
   controllers: [HL7Controller],
   providers: [HL7Service],
 })
-export class Hl7Module implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(PlainTextMiddleware).forRoutes('hl7/parse');
-  }
-}
+export class Hl7Module {};
