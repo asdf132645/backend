8343f1e1a276ada4a36bf0d4b6f6e2203e833db7
henry <kaynekaynekayne@github.com>
2024-08-12 13:37:12 +0900

    0.0.50v_(로그 형식 변경)

--- src/logger.service.ts
+++ src/logger.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as moment from 'moment';
 
 @Injectable()
 export class LoggerService extends Logger {
@@ -31,8 +32,19 @@ export class LoggerService extends Logger {
     this.writeLog('debug', message);
   }
 
+  private formattedTime = (date) => {
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+    const formattedTimeStr = `[${hours}:${minutes}:${seconds}.${milliseconds}]`;
+    return formattedTimeStr;
+  };
+
   private writeLog(level: string, message: string) {
-    const date = new Date();
+    const now = new Date();
+    const date = moment(now).toDate();
     const dateString = date.toISOString().split('T')[0]; // YYYY-MM-DD 형식으로 날짜 문자열 생성
     const logDir = path.join(this.baseLogDir, level, dateString);
 
@@ -40,7 +52,10 @@ export class LoggerService extends Logger {
 
     const logFilePath = path.join(logDir, `${dateString}.txt`);
 
-    fs.appendFileSync(logFilePath, `${date.toISOString()} - ${message}\n`);
+    fs.appendFileSync(
+      logFilePath,
+      `${this.formattedTime(date)} - ${message}\n`,
+    );
   }
 
   private ensureBaseLogDirectoryExists() {
