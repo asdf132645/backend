87a08de46097af294cf41733e9760961f54c326b
coin <amber132623@gmail.com>
2024-09-02 15:19:09 +0900

    파일 시스템 업데이트

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { promises as fs } from 'fs';
 import { writeFile, mkdir, access, constants } from 'fs/promises';
 import * as path from 'path';
+import * as fss from 'fs'; // 'fs/promises' 대신 'fs'를 사용
 
 @Injectable()
 export class FileService {
@@ -34,7 +35,10 @@ export class FileService {
       message: `File not found with any of the extensions: ${this.possibleExtensions.join(', ')}`,
     };
   }
-
+  checkFileExists(directoryPath: string, filename: string): boolean {
+    const fullPath = path.join(directoryPath, filename);
+    return fss.existsSync(fullPath);
+  }
   async cbcSaveDataService(filePath: string, data: any): Promise<void> {
     // 클라이언트가 제공한 파일 경로를 직접 사용
     const directory = path.dirname(filePath);
