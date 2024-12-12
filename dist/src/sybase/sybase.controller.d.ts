8618b186e6cf5996421ded000ec8e0ae22b6db88
henry <qooktree@gmail.com>
2024-12-12 16:51:03 +0900

    .

--- dist/src/sybase/sybase.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { SybaseProxyService } from './sybase.proxy.service';
-export declare class SybaseController {
-    private readonly sybaseProxyService;
-    constructor(sybaseProxyService: SybaseProxyService);
-    getCbcResults(smp_no: string): Promise<any>;
-    saveUimdResult(data: any): Promise<any>;
-    cbcImgGet(smp_no: string): Promise<any>;
-    saveComment(data: any): Promise<any>;
-}
--- dist/src/sybase/sybase.proxy.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class SybaseProxyService {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    getCbcResults(smp_no: string): Promise<any>;
-    saveUimdResult(data: any): Promise<any>;
-    cbcImgGet(smp_no: string): Promise<any>;
-    saveComment(data: any): Promise<any>;
-}
