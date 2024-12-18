ec684fb650c0f4fb6fe70355ce850457a59dea73
coin <amber132623@gmail.com>
2024-12-18 13:07:46 +0900

    0.0.88v_(그래프 ql 수정)

--- src/runingInfo/runningInfo.resolver.ts
+++ src/runingInfo/runningInfo.resolver.ts
@@ -1,15 +1,21 @@
 import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { RuningInfoService } from './runingInfo.service';
 import { RuningInfoEntity } from './runingInfo.entity';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
 
 @Resolver(() => RuningInfoEntity)
 export class RunningInfoResolver {
-  constructor(private readonly runningInfoService: RuningInfoService) {}
+  constructor(
+    private readonly runningInfoService: RuningInfoService,
+    @InjectRedis() private readonly redis: Redis, // Redis 인스턴스 주입
+  ) {}
 
   @Query(() => RuningInfoEntity)
   async getRunningInfoByIdGQL(
     @Args('id', { type: () => Int }) id: number, // GraphQL의 Int로 명시
   ): Promise<RuningInfoEntity> {
+    await this.redis.flushall(); // 모든 키 삭제
     const runningInfo = await this.runningInfoService.getRunningInfoById(id);
     console.log(runningInfo);
     if (runningInfo) {
